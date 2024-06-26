8460863f49b0ee4ca44ce60405b711e307933bf5
Eddy <edkosyan2000@gmail.com>
2024-06-26 18:27:34 +0400

    fix cache

--- src/store/textEditor/slice.ts
+++ src/store/textEditor/slice.ts
@@ -2,12 +2,13 @@ import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 import { EditorValueType, ITextEditorState } from "./types";
 
+const cacheSize = 10; //Maximum cached values
 
 const initialValue: EditorValueType = {
   text: "",
   bold: false,
   italic: false,
-}
+};
 
 // Define the initial state using that type
 const initialState: ITextEditorState = {
@@ -20,13 +21,18 @@ export const textEditor = createSlice({
   initialState,
   reducers: {
     addValue: (state, action: PayloadAction<EditorValueType>) => {
+      const newValue = action.payload;
       const lastValue = state.values[state.values.length - 1];
       const valuesDifferent =
-        JSON.stringify(action.payload) !== JSON.stringify(lastValue);
-      if (valuesDifferent) {
-        state.actualIndex++
-        state.values = state.values.slice(0, state.actualIndex)
-        state.values.push(action.payload);
+        JSON.stringify(newValue) !== JSON.stringify(lastValue);
+      if (!valuesDifferent) return; //Nothing to change
+      state.actualIndex++;
+      state.values = state.values.slice(0, state.actualIndex);
+      state.values.push(newValue);
+      //Crop values size according to cache
+      if (state.values.length > cacheSize) {
+        state.values.shift();
+        state.actualIndex--;
       }
     },
     setActualIndex: (state, action: PayloadAction<number>) => {
