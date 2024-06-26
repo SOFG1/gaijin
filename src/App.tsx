65799dd6c6022e5da0acea49cd59d20f88d2e25c
Eddy <edkosyan2000@gmail.com>
2024-06-26 16:13:34 +0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+
+
+  
+  return (
+    <>
+      <h1>Text editor</h1>
+      <textarea style={{display: "block", minHeight: "70px"}} />
+      <button>Undo</button>
+      <button>Redo</button>
+      <button>Bold</button>
+      <button>Italic</button>
+    </>
+  );
+}
+
+export default App;
